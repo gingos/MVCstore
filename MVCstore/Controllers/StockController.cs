b3029f461430af056bec419753cd780f7449671d
yoadwo <yoadwo@ac.sce.ac.il>
2017-01-09 23:16:34 +0200

    show stock: now adds stock requests add stock dal and view model minor functionality fixes on show products and cart

--- MVCstore/Controllers/StockController.cs
+++ MVCstore/Controllers/StockController.cs
@@ -5,18 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCstore.ModelView;
 
 namespace MVCstore.Controllers
 {
     public class StockController : Controller
     {
         ProductDal proddal;
-        stockOrderDal stockDal;
+        stockRequestDal stockDal;
+        RestockViewModel rvm;
 
         public StockController()
         {
             proddal = new ProductDal();
-            stockDal = new stockOrderDal();
+            stockDal = new stockRequestDal();
+            rvm = new RestockViewModel();
         }
 
         // GET: Stock
@@ -26,7 +29,9 @@ namespace MVCstore.Controllers
         }
 
         //TODO:
-        // 3. stock tables similar to show products
+        // 
+        // showStock with 0 quantity
+
         //showStock
         public ActionResult showStock()
         {
@@ -37,6 +42,31 @@ namespace MVCstore.Controllers
             List<Product> prodList = proddal.products.ToList();
             return Json(prodList, JsonRequestBehavior.AllowGet);
         }
+        public ActionResult getZeroQuantityByJSON()
+        {
+            List<Product> prodList = proddal.products.Where(p => p.Quantity == 0).ToList();
+            return Json(prodList, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult restockRequest(int quantityToAdd, string model)
+        {
+            StockRequest sr = new StockRequest();
+            sr.Quantity = quantityToAdd;
+            sr.Model = model;
+            stockDal.stockRequests.Add(sr);
+            stockDal.SaveChanges();
+
+            return new EmptyResult();
+        }
+        public ActionResult showConfirm()
+        {
+            //TODO: add "show confirm" view
+            // is strongly typed to request VM
+            // shows uncofirmed requests, on clock, assigns date and updates product quantity
+            rvm.sr = new StockRequest();
+            rvm.stockRequestList = stockDal.stockRequests.Where(sr => sr.arrivedDate == null).ToList<StockRequest>();
+            return View(rvm);
+        }
+
 
 
     }
---
+++ MVCstore/ModelView/RestockViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCstore.Models;
+
+namespace MVCstore.ModelView
+{
+    public class RestockViewModel
+    {
+        public StockRequest sr { get; set; }
+        public List<StockRequest> stockRequestList { get; set; }
+    }
+}
