4f40696e786ccb443550250a8b5174d0cba40066
yairschneid <yairschneid@gmail.com>
2017-01-11 17:33:46 +0200

    another try

---
+++ MVCstore/Models/Customers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MVCstore.Models
+{
+    public class Customers
+    {
+        [Required]
+        [MaxLength(10, ErrorMessage ="I don't read that much")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(10, ErrorMessage ="I don't read that much")]
+        public string LastName { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerNumber { get; set; }
+
+        [Required]
+        [RegularExpression ("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$",ErrorMessage = "what have i asked? a frikin email?")]
+        public string Email { get; set; }
+
+        [RegularExpression("[05+0-9]",ErrorMessage ="please enter a phone number starting with 05...")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        /// <summary>
+        /// Hash function to hash code the password of the user, using MD5 algorithm
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>ToBase64String of hashBytes</returns>
+        public void MD5Hash()
+        {
+            var bytes = new UTF8Encoding().GetBytes(PasswordHash);
+            var hashBytes = System.Security.Cryptography.MD5.Create().ComputeHash(bytes);
+            Convert.ToBase64String(hashBytes);
+        }
+    }
+}
