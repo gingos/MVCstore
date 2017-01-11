68b097d25c7cc116dfac427b42e298b75a1221fb
yairschneid <yairschneid@gmail.com>
2017-01-11 16:15:19 +0200

    new connetcion string to CustomersDal fixed register page

--- MVCstore/Controllers/HomeController.cs
+++ MVCstore/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-﻿using MVCstore.Models;
+﻿using MVCstore.DAL;
+using MVCstore.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +13,8 @@ namespace MVCstore.Controllers
 {
     public class HomeController : Controller
     {
+        CustomersDal custDal;
+
         public ActionResult Index()
         {
             return View();
@@ -36,18 +42,47 @@ namespace MVCstore.Controllers
         public ActionResult Register()
         {
             
-            return View("Register", new Register());
+            return View("Register", new Customers());
         }
 
-        public ActionResult Submit(Register reg)
+       
+        public ActionResult SubmitRegister(Customers reg)
         {
-            if (ModelState.IsValid)
-                return View("Login");
+            string hash = reg.MD5Hash(reg.PasswordHash);
+            reg.PasswordHash = hash;
+            custDal = new CustomersDal();
+
+            if (ModelState.IsValid) {
+                Customers cust = new Customers
+                {
+                    FirstName = reg.FirstName,
+                    LastName = reg.LastName,
+                    Email = reg.Email,
+                    PasswordHash = hash,
+                    PhoneNumber = reg.PhoneNumber                    
+                };
+
+                custDal.customers.Add(cust);
+                custDal.SaveChanges();              
+
+                return RedirectToAction("Index", cust);
+            }
             else
                 return View("Register", reg);
         }
 
-      
+        public ActionResult SubmitLogin(Customers reg)
+        {
+            if (ModelState.IsValid)
+            {
+                string hash = reg.MD5Hash(reg.PasswordHash);
+                
+                return View("Index", reg);
+            }
+            else
+                return View("Register", reg);
+        }
+
 
     }
 }
---
+++ MVCstore/DAL/CustomersDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MVCstore.Models;
+
+namespace MVCstore.DAL
+{
+    public class CustomersDal : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Customers>().ToTable("Customers");
+        }
+        public DbSet<Customers> customers { get; set; }
+    }
+}
